c38715b1349673b3b51bf8872fc9110ace393c17
rbaduwal <103967137+rbaduwal@users.noreply.github.com>
2022-06-13 18:36:57 -0700

    Initial Commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Rajesh Baduwal on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
